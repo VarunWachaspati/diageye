816343c04781fd37273a350323e2563deaba45ad
VarunWachaspati <varunwachaspati@gmail.com>
2015-06-16 16:20:13 +0530

    Added/Created a new object for Retina and InjectionSite is now inside the control of Canvas

--- assets/js/dist/eyedraw.js
+++ assets/js/dist/eyedraw.js
@@ -14256,6 +14256,167 @@ ED.AntPVR.prototype.diagnosticHierarchy = function() {
 }
 
 /**
+ * Lattice @author - Varun
+ *
+ * @class Lattice
+ * @property {String} className Name of doodle subclass
+ * @param {Drawing} _drawing
+ * @param {Object} _parameterJSON
+ */
+ED.Lattice = function(_drawing, _parameterJSON) {
+	// Set classname
+	this.className = "Lattice";
+
+	// Saved parameters
+	this.savedParameterArray = ['apexY', 'arc', 'rotation'];
+
+	// Call super-class constructor
+	ED.Doodle.call(this, _drawing, _parameterJSON);
+}
+
+/**
+ * Sets superclass and constructor
+ */
+ED.Lattice.prototype = new ED.Doodle;
+ED.Lattice.prototype.constructor = ED.Lattice;
+ED.Lattice.superclass = ED.Doodle.prototype;
+
+/**
+ * Sets handle attributes
+ */
+ED.Lattice.prototype.setHandles = function() {
+	this.handleArray[0] = new ED.Doodle.Handle(null, true, ED.Mode.Arc, false);
+	this.handleArray[3] = new ED.Doodle.Handle(null, true, ED.Mode.Arc, false);
+	this.handleArray[4] = new ED.Doodle.Handle(null, true, ED.Mode.Apex, false);
+}
+
+/**
+ * Sets default dragging attributes
+ */
+ED.Lattice.prototype.setPropertyDefaults = function() {
+	this.isMoveable = false;
+
+	// Update component of validation array for simple parameters
+	this.parameterValidationArray['apexX']['range'].setMinAndMax(-0, +0);
+	this.parameterValidationArray['apexY']['range'].setMinAndMax(-400, -300);
+	this.parameterValidationArray['scaleX']['range'].setMinAndMax(+0.25, +4);
+	this.parameterValidationArray['scaleY']['range'].setMinAndMax(+0.25, +4);
+	this.parameterValidationArray['arc']['range'].setMinAndMax(Math.PI / 9, Math.PI * 2);
+}
+
+
+/**
+ * Sets default parameters
+ */
+ED.Lattice.prototype.setParameterDefaults = function() {
+	this.arc =   Math.PI / 3;
+	this.apexY = -300;
+	this.setRotationWithDisplacements(180, 120);
+}
+
+/**
+ * Draws doodle or performs a hit test if a Point parameter is passed
+ *
+ * @param {Point} _point Optional point in canvas plane, passed if performing hit test
+ */
+ED.Lattice.prototype.draw = function(_point) {
+	// Get context
+	var ctx = this.drawing.context;
+
+	// Call draw method in superclass
+	ED.Lattice.superclass.draw.call(this, _point);
+
+	// Radius of outer curve just inside ora on right and left fundus diagrams
+	var ro = 952 / 2;
+	var ri = -this.apexY;
+	var r = ri + (ro - ri) / 2;
+
+	// Calculate parameters for arcs
+	var theta = this.arc / 2;
+	var arcStart = -Math.PI / 2 + theta;
+	var arcEnd = -Math.PI / 2 - theta;
+
+	// Coordinates of 'corners' of lattice
+	var topRightX = r * Math.sin(theta);
+	var topRightY = -r * Math.cos(theta);
+	var topLeftX = -r * Math.sin(theta);
+	var topLeftY = topRightY;
+
+	// Boundary path
+	ctx.beginPath();
+
+	// Arc across to mirror image point on the other side
+	ctx.arc(0, 0, ro, arcStart, arcEnd, true);
+
+	// Arc back to mirror image point on the other side
+	ctx.arc(0, 0, ri, arcEnd, arcStart, false);
+
+	// Close path
+	ctx.closePath();
+
+	// Set line attributes
+	ctx.lineWidth = 4;
+	var ptrn = ctx.createPattern(this.drawing.imageArray['LatticePattern'], 'repeat');
+	ctx.fillStyle = ptrn;
+	ctx.strokeStyle = "gray";
+
+	// Draw boundary path (also hit testing)
+	this.drawBoundary(_point);
+
+	// Coordinates of handles (in canvas plane)
+	this.handleArray[0].location = this.transform.transformPoint(new ED.Point(topLeftX, topLeftY));
+	this.handleArray[3].location = this.transform.transformPoint(new ED.Point(topRightX, topRightY));
+	this.handleArray[4].location = this.transform.transformPoint(new ED.Point(this.apexX, this.apexY));
+
+	// Draw handles if selected
+	if (this.isSelected && !this.isForDrawing) this.drawHandles(_point);
+
+	// Return value indicating successful hit test
+	return this.isClicked;
+}
+
+ED.Lattice.prototype.description = function() {
+	returnString =  "Lattice ";
+
+		// Use trigonometry on rotation field to get clock hour of start (0.2618 is PI/12)
+	var start = this.rotation - this.arc / 2;
+	var clockHour = Math.floor((((start + 0.2618) * 6 / Math.PI) + 12) % 12);
+	if (clockHour == 0) clockHour = 12;
+
+	// Get extent of tear in degrees
+	var extent = (this.arc * 180 / Math.PI);
+
+	// Round to nearest 10
+	extent = 10 * Math.floor((extent + 5) / 10);
+
+	returnString = returnString + extent + " degrees clockwise from " + clockHour + " o'clock";
+
+	return returnString;
+}
+
+
+
+/** **TODO** - Find the SnoMed Code for Lattice
+ * Returns the SnoMed code of the doodle
+ *
+ * @returns {Int} SnoMed code of entity representated by doodle
+ */
+ED.Lattice.prototype.snomedCode = function() {
+	return 0;
+}
+
+/**
+ * Returns a number indicating position in a hierarchy of diagnoses from 0 to 9 (highest)
+ *
+ * @returns {Int} Position in diagnostic hierarchy
+ */
+ED.Lattice.prototype.diagnosticHierarchy = function() {
+	return 2;
+}
+
+
+
+/**
  * OpenEyes
  *
  * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
@@ -25427,14 +25588,14 @@ ED.InjectionSite.prototype.draw = function(_point) {
 
 	// Hub
 	var y = this.parsPlana - 300;
-	ctx.moveTo(-80, y);
-	ctx.lineTo(80, y);
-	ctx.lineTo(70, y + 30);
-	ctx.lineTo(50, y + 240);
-	ctx.lineTo(20, y + 260);
-	ctx.lineTo(-20, y + 260);
-	ctx.lineTo(-50, y + 240);
-	ctx.lineTo(-70, y + 30);
+	ctx.moveTo(-80, y + 110);//added 110 @author: Varun
+	ctx.lineTo(80, y + 110);//added 110
+	ctx.lineTo(70, y + 30 + 110);//added 110
+	ctx.lineTo(50, y + 240 + 110);//added 110
+	ctx.lineTo(20, y + 260 + 110);//added 110
+	ctx.lineTo(-20, y + 260 + 110);//added 110
+	ctx.lineTo(-50, y + 240 + 110);//added 110
+	ctx.lineTo(-70, y + 30 + 110);//added 110
 	ctx.closePath();
 
 	// Set line attributes
@@ -25451,34 +25612,34 @@ ED.InjectionSite.prototype.draw = function(_point) {
 
 		// Hub top
 		ctx.beginPath();
-		ctx.moveTo(-70, y + 30);
-		ctx.lineTo(70, y + 30);
+		ctx.moveTo(-70, y + 30 + 160);//added 160
+		ctx.lineTo(70, y + 30 + 160);//added 160
 		ctx.lineWidth = 1;
 		ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
 		ctx.stroke();
 
 		// Ridge
 		ctx.beginPath();
-		ctx.moveTo(-60, y + 130);
-		ctx.lineTo(60, y + 130);
+		ctx.moveTo(-60, y + 130 + 160);//added 160
+		ctx.lineTo(60, y + 130 + 160);//added 160
 		ctx.lineWidth = 1;
 		ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
 		ctx.stroke();
 
 		// Flange
 		ctx.beginPath();
-		ctx.moveTo(0, y + 130);
-		ctx.lineTo(0, y + 230);
+		ctx.moveTo(0, y + 130 + 160);//added 160
+		ctx.lineTo(0, y + 230 + 160);//added 160
 		ctx.lineWidth = 32;
 		ctx.strokeStyle = "rgba(120, 120, 120, 0.2)";
 		ctx.stroke();
 
 		// White bit
 		ctx.beginPath();
-		ctx.moveTo(50, y + 240);
-		ctx.lineTo(20, y + 260);
-		ctx.lineTo(-20, y + 260);
-		ctx.lineTo(-50, y + 240);
+		ctx.moveTo(50, y + 240 + 110);//added 110
+		ctx.lineTo(20, y + 260 + 110);//added 110
+		ctx.lineTo(-20, y + 260 + 110);//added 110
+		ctx.lineTo(-50, y + 240 + 110);//added 110
 		ctx.closePath();
 		ctx.lineWidth = 1;
 		ctx.fillStyle = "rgba(255, 255, 255, 1)";
@@ -25488,8 +25649,8 @@ ED.InjectionSite.prototype.draw = function(_point) {
 
 		// Visible needle
 		ctx.beginPath();
-		ctx.moveTo(-10, y + 260);
-		ctx.lineTo(+10, y + 260);
+		ctx.moveTo(-10, y + 260);//added 260
+		ctx.lineTo(+10, y + 260);//added 260
 		ctx.lineTo(+10, this.apexY);
 		ctx.lineTo(-10, this.apexY);
 		ctx.closePath();
@@ -25500,8 +25661,8 @@ ED.InjectionSite.prototype.draw = function(_point) {
 		ctx.beginPath();
 		ctx.moveTo(-10, this.apexY);
 		ctx.lineTo(+10, this.apexY);
-		ctx.lineTo(+10, -130);
-		ctx.lineTo(-10, -100);
+		ctx.lineTo(+10, -130 - 260); // Subtracted 260 
+		ctx.lineTo(-10, -100 - 260); // Subtracted 260 
 		ctx.closePath();
 		ctx.lineWidth = 1;
 		ctx.strokeStyle = "rgba(60, 60, 60, 1)";
