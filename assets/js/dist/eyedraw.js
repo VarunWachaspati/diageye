f6ad88870f796727d8e08c9d639d6971b3cdb0c0
VarunWachaspati <varunwachaspati@gmail.com>
2015-06-20 15:24:34 +0530

    added new object siliconeOil

--- assets/js/dist/eyedraw.js
+++ assets/js/dist/eyedraw.js
@@ -34942,6 +34942,204 @@ ED.RRD.prototype.isMacOff = function() {
 }
 
 /**
+ * Silicone Oil
+ * @author: Varun
+ * @class SiliconeOil
+ * @property {String} className Name of doodle subclass
+ * @param {Drawing} _drawing
+ * @param {Object} _parameterJSON
+ */
+ED.SiliconeOil = function(_drawing, _parameterJSON) {
+	// Set classname
+	this.className = "SiliconeOil";
+
+	// Saved parameters
+	this.savedParameterArray = ['apexY', 'arc', 'rotation'];
+
+	// Call superclass constructor
+	ED.Doodle.call(this, _drawing, _parameterJSON);
+}
+
+/**
+ * Sets superclass and constructor
+ */
+ED.SiliconeOil.prototype = new ED.Doodle;
+ED.SiliconeOil.prototype.constructor = ED.SiliconeOil;
+ED.SiliconeOil.superclass = ED.Doodle.prototype;
+
+/**
+ * Sets handle attributes
+ */
+ED.SiliconeOil.prototype.setHandles = function() {
+	//this.handleArray[1] = new ED.Doodle.Handle(null, true, ED.Mode.Arc, false);
+	//this.handleArray[2] = new ED.Doodle.Handle(null, true, ED.Mode.Arc, false);
+	//this.handleArray[4] = new ED.Doodle.Handle(null, true, ED.Mode.Apex, false);
+}
+
+/**
+ * Sets default properties
+ */
+ED.SiliconeOil.prototype.setPropertyDefaults = function() {
+	this.isMoveable = false;
+
+	// Update component of validation array for simple parameters
+	this.parameterValidationArray['rotation']['range'].setMinAndMax( +155*Math.PI/180, +205*Math.PI/180);
+ 	this.parameterValidationArray['scaleX']['range'].setMinAndMax(0, +4);
+ 	this.parameterValidationArray['scaleY']['range'].setMinAndMax(0, +4);
+	this.parameterValidationArray['apexX']['range'].setMinAndMax(-0, +0);
+	this.parameterValidationArray['apexY']['range'].setMinAndMax(-400, -100);
+}
+
+/**
+ * Sets default parameters (Only called for new doodles)
+ * Use the setParameter function for derived parameters, as this will also update dependent variables
+ */
+ED.SiliconeOil.prototype.setParameterDefaults = function() {
+	this.arc = 120 * Math.PI / 180;
+	this.apexY = -365;
+	this.setRotationWithDisplacements(185, 180);
+}
+
+/**
+ * Draws doodle or performs a hit test if a Point parameter is passed
+ *
+ * @param {Point} _point Optional point in canvas plane, passed if performing hit test
+ */
+ED.SiliconeOil.prototype.draw = function(_point) {
+	// Get context
+	var ctx = this.drawing.context;
+
+	// Call draw method in superclass
+	ED.SiliconeOil.superclass.draw.call(this, _point);
+
+	// Fit outer curve just inside ora on right and left fundus diagrams
+	var r = 952 / 2;
+
+	// Calculate parameters for arcs
+	var theta = this.arc / 2;
+	var arcStart = -Math.PI / 2 + theta;
+	var arcEnd = -Math.PI / 2 - theta;
+
+	// Coordinates of corners of arc
+	var topRightX = r * Math.sin(theta);
+	var topRightY = -r * Math.cos(theta);
+	var topLeftX = -r * Math.sin(theta);
+	var topLeftY = topRightY;
+
+	// Boundary path
+	ctx.beginPath();
+
+	// Arc across from top right to to mirror image point on the other side
+	ctx.arc(0, 0, r, arcStart, arcEnd, true);
+
+	// Connect across the bottom via the apex point
+	var bp = +0.6;
+
+	// Radius of disc (from Fundus doodle)
+	var dr = +25;
+
+	// RD above optic disc
+	if (this.apexY < -dr) {
+		ctx.bezierCurveTo(topLeftX, topLeftY, bp * topLeftX, this.apexY, this.apexX, this.apexY);
+		ctx.bezierCurveTo(-bp * topLeftX, this.apexY, topRightX, topRightY, topRightX, topRightY);
+	}
+	// SiliconeOil involves optic disc
+	else if (this.apexY < dr) {
+		// Angle from origin to intersection of disc margin with a horizontal line through apexY
+		var phi = Math.acos((0 - this.apexY) / dr);
+
+		// Curve to disc, curve around it, then curve out again
+		var xd = dr * Math.sin(phi);
+		ctx.bezierCurveTo(topLeftX, topLeftY, bp * topLeftX, this.apexY, -xd, this.apexY);
+		ctx.arc(0, 0, dr, -Math.PI / 2 - phi, -Math.PI / 2 + phi, false);
+		ctx.bezierCurveTo(-bp * topLeftX, this.apexY, topRightX, topRightY, topRightX, topRightY);
+	}
+	// SiliconeOil beyond optic disc
+	else {
+		ctx.bezierCurveTo(topLeftX, topLeftY, bp * topLeftX, this.apexY, 0, 25);
+		ctx.arc(0, 0, dr, Math.PI / 2, 2.5 * Math.PI, false);
+		ctx.bezierCurveTo(-bp * topLeftX, this.apexY, topRightX, topRightY, topRightX, topRightY);
+	}
+
+	// Set line attributes
+	ctx.lineWidth = 4;
+	ctx.fillStyle = "rgba(27, 245, 78, 0.85)";
+	ctx.strokeStyle = "green";
+
+	// Draw boundary path (also hit testing)
+	this.drawBoundary(_point);
+
+	// Coordinates of handles (in canvas plane)
+	//this.handleArray[1].location = this.transform.transformPoint(new ED.Point(topLeftX, topLeftY));
+	//this.handleArray[2].location = this.transform.transformPoint(new ED.Point(topRightX, topRightY));
+	//this.handleArray[4].location = this.transform.transformPoint(new ED.Point(this.apexX, this.apexY));
+
+	// Draw handles if selected
+	if (this.isSelected && !this.isForDrawing) this.drawHandles(_point);
+
+	// Return value indicating successful hittest
+	return this.isClicked;
+}
+
+/**
+ * Returns a string containing a text description of the doodle
+ *
+ * @returns {String} Description of doodle
+ */
+ED.SiliconeOil.prototype.description = function() {
+	// Construct description
+	var returnString = "";
+
+	// Use trigonometry on rotation field to determine quadrant
+	returnString = returnString + (Math.cos(this.rotation) > 0 ? "Supero" : "Infero");
+	returnString = returnString + (Math.sin(this.rotation) > 0 ? (this.drawing.eye == ED.eye.Right ? "nasal" : "temporal") : (this.drawing.eye == ED.eye.Right ? "temporal" : "nasal"));
+	returnString = returnString + " Silicone Oil";
+	//returnString = returnString + (this.isMacOff() ? " (macula off)" : " (macula on)");
+
+	// Return description
+	return returnString;
+}
+
+/**
+ * Returns the SnoMed code of the doodle
+ *
+ * @returns {Int} SnoMed code of entity representated by doodle
+ */
+ED.SiliconeOil.prototype.snomedCode = function() {
+	return 0;
+}
+
+/**
+ * Returns a number indicating position in a hierarchy of diagnoses from 0 to 9 (highest)
+ *
+ * @returns {Int} Position in diagnostic hierarchy
+ */
+ED.SiliconeOil.prototype.diagnosticHierarchy = function() {
+	return 0;
+}
+
+/**
+ * Determines whether the macula is off or not
+ *
+ * @returns {Bool} True if macula is off
+ */
+ED.SiliconeOil.prototype.isMacOff = function() {
+	// Get coordinates of macula in doodle plane
+	if (this.drawing.eye == ED.eye.Right) {
+		var macula = new ED.Point(-100, 0);
+	} else {
+		var macula = new ED.Point(100, 0);
+	}
+
+	// Convert to canvas plane
+	var maculaCanvas = this.drawing.transform.transformPoint(macula);
+
+	// Determine whether macula is off or not
+	if (this.draw(maculaCanvas)) return true;
+	else return false;
+}
+
+/**
  * OpenEyes
  *
  * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
